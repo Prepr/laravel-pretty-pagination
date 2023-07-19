1e213b6c4f26c1a1243abdcfe6578e670318538c
Hemarao Dugana <hemarao@prepr.org>
2023-07-19 18:36:40 +0530

    code push for initial commit

---
+++ CONTRIBUTING.md
@@ -0,0 +1,14 @@
+# Contributing
+
+Contributions are **welcome** and will be fully **credited**.
+
+We accept contributions via Pull Requests on [Github](https://github.com/prepr/laravel-pretty-pagination).
+
+## Running Tests
+
+``` bash
+$ phpunit
+```
+
+
+**Happy coding**!
---
+++ src/PaginateRouteFacade.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Prepr\PaginateRoute;
+
+use Illuminate\Support\Facades\Facade;
+
+class PaginateRouteFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'paginateroute';
+    }
+}
